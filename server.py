6aad0a771e6b75c020b9358a4bc149dd4beab7f6
Veera <veerakarthick609@gmail.com>
2023-12-26 13:59:03 +0530

    updates

--- server.py
+++ server.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, jsonify
 import util
 
-app = Flask(__name__)
+server = Flask(__name__)
 
-@app.route('/get_location_names', methods=['GET'])
+@server.route('/get_location_names', methods=['GET'])
 def get_location_names():
     response = jsonify({
         'locations': util.get_location_names()
@@ -12,7 +12,7 @@ def get_location_names():
 
     return response
 
-@app.route('/predict_home_price', methods=['GET', 'POST'])
+@server.route('/predict_home_price', methods=['GET', 'POST'])
 def predict_home_price():
     total_sqft = float(request.form['total_sqft'])
     location = request.form['location']
@@ -29,4 +29,4 @@ def predict_home_price():
 if __name__ == "__main__":
     print("Starting Python Flask Server For Home Price Prediction...")
     util.load_saved_artifacts()
-    app.run(debug=True)
+    server.run(debug=True)

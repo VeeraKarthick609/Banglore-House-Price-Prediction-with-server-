6ecd5fdec9301101173e64a8861e38d9b44f7d84
Veera <veerakarthick609@gmail.com>
2023-12-27 08:14:17 +0530

    updates

--- server.py
+++ server.py
@@ -1,32 +1,296 @@
 from flask import Flask, request, jsonify
-import util
+import numpy as np
+import pickle  # Importing pickle for loading the model
 
 server = Flask(__name__)
+
+all_locations = [
+        "1st block jayanagar",
+        "1st phase jp nagar",
+        "2nd phase judicial layout",
+        "2nd stage nagarbhavi",
+        "5th block hbr layout",
+        "5th phase jp nagar",
+        "6th phase jp nagar",
+        "7th phase jp nagar",
+        "8th phase jp nagar",
+        "9th phase jp nagar",
+        "aecs layout",
+        "abbigere",
+        "akshaya nagar",
+        "ambalipura",
+        "ambedkar nagar",
+        "amruthahalli",
+        "anandapura",
+        "ananth nagar",
+        "anekal",
+        "anjanapura",
+        "ardendale",
+        "arekere",
+        "attibele",
+        "beml layout",
+        "btm 2nd stage",
+        "btm layout",
+        "babusapalaya",
+        "badavala nagar",
+        "balagere",
+        "banashankari",
+        "banashankari stage ii",
+        "banashankari stage iii",
+        "banashankari stage v",
+        "banashankari stage vi",
+        "banaswadi",
+        "banjara layout",
+        "bannerghatta",
+        "bannerghatta road",
+        "basavangudi",
+        "basaveshwara nagar",
+        "battarahalli",
+        "begur",
+        "begur road",
+        "bellandur",
+        "benson town",
+        "bharathi nagar",
+        "bhoganhalli",
+        "billekahalli",
+        "binny pete",
+        "bisuvanahalli",
+        "bommanahalli",
+        "bommasandra",
+        "bommasandra industrial area",
+        "bommenahalli",
+        "brookefield",
+        "budigere",
+        "cv raman nagar",
+        "chamrajpet",
+        "chandapura",
+        "channasandra",
+        "chikka tirupathi",
+        "chikkabanavar",
+        "chikkalasandra",
+        "choodasandra",
+        "cooke town",
+        "cox town",
+        "cunningham road",
+        "dasanapura",
+        "dasarahalli",
+        "devanahalli",
+        "devarachikkanahalli",
+        "dodda nekkundi",
+        "doddaballapur",
+        "doddakallasandra",
+        "doddathoguru",
+        "domlur",
+        "dommasandra",
+        "epip zone",
+        "electronic city",
+        "electronic city phase ii",
+        "electronics city phase 1",
+        "frazer town",
+        "gm palaya",
+        "garudachar palya",
+        "giri nagar",
+        "gollarapalya hosahalli",
+        "gottigere",
+        "green glen layout",
+        "gubbalala",
+        "gunjur",
+        "hal 2nd stage",
+        "hbr layout",
+        "hrbr layout",
+        "hsr layout",
+        "haralur road",
+        "harlur",
+        "hebbal",
+        "hebbal kempapura",
+        "hegde nagar",
+        "hennur",
+        "hennur road",
+        "hoodi",
+        "horamavu agara",
+        "horamavu banaswadi",
+        "hormavu",
+        "hosa road",
+        "hosakerehalli",
+        "hoskote",
+        "hosur road",
+        "hulimavu",
+        "isro layout",
+        "itpl",
+        "iblur village",
+        "indira nagar",
+        "jp nagar",
+        "jakkur",
+        "jalahalli",
+        "jalahalli east",
+        "jigani",
+        "judicial layout",
+        "kr puram",
+        "kadubeesanahalli",
+        "kadugodi",
+        "kaggadasapura",
+        "kaggalipura",
+        "kaikondrahalli",
+        "kalena agrahara",
+        "kalyan nagar",
+        "kambipura",
+        "kammanahalli",
+        "kammasandra",
+        "kanakapura",
+        "kanakpura road",
+        "kannamangala",
+        "karuna nagar",
+        "kasavanhalli",
+        "kasturi nagar",
+        "kathriguppe",
+        "kaval byrasandra",
+        "kenchenahalli",
+        "kengeri",
+        "kengeri satellite town",
+        "kereguddadahalli",
+        "kodichikkanahalli",
+        "kodigehaali",
+        "kodigehalli",
+        "kodihalli",
+        "kogilu",
+        "konanakunte",
+        "koramangala",
+        "kothannur",
+        "kothanur",
+        "kudlu",
+        "kudlu gate",
+        "kumaraswami layout",
+        "kundalahalli",
+        "lb shastri nagar",
+        "laggere",
+        "lakshminarayana pura",
+        "lingadheeranahalli",
+        "magadi road",
+        "mahadevpura",
+        "mahalakshmi layout",
+        "mallasandra",
+        "malleshpalya",
+        "malleshwaram",
+        "marathahalli",
+        "margondanahalli",
+        "marsur",
+        "mico layout",
+        "munnekollal",
+        "murugeshpalya",
+        "mysore road",
+        "ngr layout",
+        "nri layout",
+        "nagarbhavi",
+        "nagasandra",
+        "nagavara",
+        "nagavarapalya",
+        "narayanapura",
+        "neeladri nagar",
+        "nehru nagar",
+        "ombr layout",
+        "old airport road",
+        "old madras road",
+        "padmanabhanagar",
+        "pai layout",
+        "panathur",
+        "parappana agrahara",
+        "pattandur agrahara",
+        "poorna pragna layout",
+        "prithvi layout",
+        "r.t. nagar",
+        "rachenahalli",
+        "raja rajeshwari nagar",
+        "rajaji nagar",
+        "rajiv nagar",
+        "ramagondanahalli",
+        "ramamurthy nagar",
+        "rayasandra",
+        "sahakara nagar",
+        "sanjay nagar",
+        "sarakki nagar",
+        "sarjapur",
+        "sarjapur  road",
+        "sarjapura - attibele road",
+        "sector 2 hsr layout",
+        "sector 7 hsr layout",
+        "seegehalli",
+        "shampura",
+        "shivaji nagar",
+        "singasandra",
+        "somasundara palya",
+        "sompura",
+        "sonnenahalli",
+        "subramanyapura",
+        "sultan palaya",
+        "tc palaya",
+        "talaghattapura",
+        "thanisandra",
+        "thigalarapalya",
+        "thubarahalli",
+        "thyagaraja nagar",
+        "tindlu",
+        "tumkur road",
+        "ulsoor",
+        "uttarahalli",
+        "varthur",
+        "varthur road",
+        "vasanthapura",
+        "vidyaranyapura",
+        "vijayanagar",
+        "vishveshwarya layout",
+        "vishwapriya layout",
+        "vittasandra",
+        "whitefield",
+        "yelachenahalli",
+        "yelahanka",
+        "yelahanka new town",
+        "yelenahalli",
+        "yeshwanthpur"
+    ]
+
+# Load the model during startup
+with open("./model/banglore_home_prices_model.pickle", "rb") as model_file:
+    model = pickle.load(model_file)
+
+def get_estimated_price(location, total_sqft, bhk, bath):
+    x = np.zeros(len(all_locations) + 3)
+    loc_index = all_locations.index(location)
+    x[0] = total_sqft
+    x[1] = bath
+    x[2] = bhk
+    x[loc_index] = 1
+
+    return model.predict([x])[0]
+
+@server.route('/predict_home_price', methods=['POST'])
+def predict_home_price():
+    try:
+        total_sqft = float(request.form['total_sqft'])
+        location = request.form['location']
+        bhk = int(request.form['bhk'])
+        bath = int(request.form['bath'])
+
+        estimated_price = get_estimated_price(location, total_sqft, bhk, bath)
+
+        response = jsonify({
+            'estimated_price': estimated_price
+        })
+
+        return response
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
 
 @server.route('/get_location_names', methods=['GET'])
 def get_location_names():
     response = jsonify({
-        'locations': util.get_location_names()
+        'locations': all_locations
     })
-    response.headers.add('Access-Control-Allow-Origin', '*')
-
-    return response
-
-@server.route('/predict_home_price', methods=['GET', 'POST'])
-def predict_home_price():
-    total_sqft = float(request.form['total_sqft'])
-    location = request.form['location']
-    bhk = int(request.form['bhk'])
-    bath = int(request.form['bath'])
-
-    response = jsonify({
-        'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
-    })
-    response.headers.add('Access-Control-Allow-Origin', '*')
 
     return response
 
 if __name__ == "__main__":
     print("Starting Python Flask Server For Home Price Prediction...")
-    util.load_saved_artifacts()
-    server.run(host="0.0.0.0",debug=False)
+    server.run(host="0.0.0.0", debug=False)
+
+
